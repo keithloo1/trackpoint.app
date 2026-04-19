1e0f28c50241642af5a76759e7f8fe4407370c04
Keith Loo <keithlck1@gmail.com>
2026-04-19 11:57:26 +0800

    Update Pocketbase API URL to use environment variable for better configuration management

--- apps/web/src/lib/pocketbaseClient.js
+++ apps/web/src/lib/pocketbaseClient.js
@@ -1,6 +1,7 @@
 import Pocketbase from 'pocketbase';
 
-const POCKETBASE_API_URL = "https://trackpoint.app/hcgi/platform";
+const POCKETBASE_API_URL =
+	import.meta.env.VITE_POCKETBASE_URL ?? 'https://trackpoint.app/hcgi/platform';
 
 const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
 
